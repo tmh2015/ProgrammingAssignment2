ceaf787a7b78682dd4531ab4e0a3ec2605f51e4d
tmh2015 <tan.minghui86@gmail.com>
2015-11-22 14:52:27 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## x is the a square invertible matrix
+  ## return will be a list containing the following functions to
+  ## set and get the matrix before setting and getting the inverse for input to cacheSolve()
+  
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    # assigning a value to x in an environment different from the current environment
+    inv <<- NULL
+    # assigning a NULL to variable inv in an environment different from the current environment
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## assigning to makeCacheMatrix()
+  ## return the inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  
+  if (!is.null(inv)){
+    # if the value of inv is not NULL, draw the data directly get it from the cache and skip computation. 
+    message("cached data has been retrieved")
+    return(inv)
+  }
+  
+  # if the value of inv is NULL, the following computation occurs.
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse via the function setinv
+  x$setinv(inv)
+  
+  return(inv)
 }
